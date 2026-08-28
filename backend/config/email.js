1849dbd4faa5b4bfad3094445a4893116bb91975
nutan23 <nutan23hcompe@student.mes.ac.in>
2026-08-28 20:24:27 +0530

    Fix email SMTP for Render

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,12 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+
+    tls: {
+        rejectUnauthorized: false
+    },
+
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 20000
 });
 
 module.exports = transporter;
